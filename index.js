2663c070f75a5cf2b7a1a1f874a12f956e93a495
Sivamani Varun <varun@varun-debian>
2014-03-15 23:02:12 -0700

    skeleton code

---
+++ index.js
@@ -0,0 +1,34 @@
+var restify = require('restify');
+
+var server = restify.createServer({
+  name: '2048-as-a-service',
+  version: '0.1.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/start', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+
+server.get('/state/:session_id', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+
+server.get('/state/:session_id/move/:move', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+
+server.get('/score/:session_id', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
