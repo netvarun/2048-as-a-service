e547d02dd7e0fddc938544646c1da3543d4644a9
Sivamani Varun <varun@varun-debian>
2014-03-30 21:41:24 -0700

    version 0.2

--- client/client.pl
+++ client/client.pl
@@ -9,25 +9,34 @@ use Data::Dumper;
 my $host = "http://localhost:8080/";
 
 #--Start game
-my $gameRef = decode_json(get($host."start"));
+my $gameRef = decode_json(get($host."start/json/thanks"));
 my $session_id = $gameRef->{'session_id'};
 my $gridRef = $gameRef->{'grid'};
 my $score = $gameRef->{'score'};
 while(1) {
-    print STDERR Dumper($gridRef),"\n";
+    prettyPrint( $gridRef );
     print STDERR "Score: $score\n";
     print STDERR "Input:\n";
     my $userInput =  <STDIN>;
     chomp ($userInput);
     print "User typed $userInput\n";
     eval{
-    $gameRef = decode_json(get($host."state/$session_id/move/$userInput"));
+    $gameRef = decode_json(get($host."state/$session_id/move/$userInput/json/thanks"));
     }; if($@) { print STDERR "Invalid move...\n"; next; }
     $gridRef = $gameRef->{'grid'};
     $score = $gameRef->{'score'};
 }
 
-
-
+sub prettyPrint {
+    my( $gridRef ) = @_;
+    my @gridArr = @{$gridRef};
+    for(my $row = 0; $row < scalar @gridArr; $row++) {
+        my @colArr = @{$gridArr[$row]};
+        foreach(@colArr) {
+            print STDERR $_, "    ";
+        }
+        print STDERR "\n";
+    }
+}
 
 
--- index.js
+++ index.js
@@ -1,63 +1,178 @@
+//TODO:
+//0. Safety checks (done)
+//1. Add timeout to clear out expired game states (done)
+//2. Check for valid session ids (done)
+//3. IP based throttling (done)
+//4. Thanks REST (done)
+//5. Error codes (done)
+//6. Redirect after start/ to state: (done)
+//7. Beautify function (done)
+//8. Mod the game (done). 
+//9. Quotes (done)
+//10. bunyan logging
+//11. params -max_games, -timeout
+//12. testing
+//13. Update docs
+
 var restify = require('restify');
 var util = require('util');
 var uuid = require('node-uuid');
 var crypto = require('crypto');
 var Game = require("./lib/game_manager.js").GameManager;
+var starwars = require('starwars');
+var Table = require('cli-table');
 
 var gameStates = {};
+var MAX_GAMES = 200000;
+var TIMEOUT = 300;
+var gameCount = 0;
 
 var server = restify.createServer({
     name: '2048-as-a-service',
     version: '0.1.0'
 });
+server.use(
+    restify.throttle({
+        burst: 100,
+        rate: 50,
+        xff: true,
+   })
+);
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/start', function (req, res, next) {
+//Check if client has thanked you
+//Obey ZEST principles
+server.pre(function(req, res, next) {
+    if(!((req.url).match(/\/thanks$/))) {
+        console.log("No thanks...");
+        res.send(800, new Error('Please say thanks...'));
+        return next();
+    }
+    return next();
+});
+
+function startGame(req, res, next) {
+    if(gameCount > MAX_GAMES) {
+        res.send(503, new Error('2048-as-a-service is running at max capacity. Sorry about that! Please try again in 5 minutes.'));
+        return next();
+    }
     var session_id = genSession();
-    gameStates[session_id] = new Game(4,2,2048);
 
-    //generate session_id
-    var gameState = gameStates[session_id].getState();
-    gameState['session_id'] = session_id;
+    //Default game params
+    var size = 4;
+    var tiles = 2;
+    var victory = 11;
+    var rand = 4;
 
-    res.send(gameState);
+    //Custom game
+    if((req.url).match(/\/size\//)) {
+        size = parseInt(req.params.size);
+        tiles = parseInt(req.params.tiles);
+        victory = parseInt(req.params.victory);
+        rand = parseInt(req.params.rand);
+
+        if(size < 3 || size > 16) {
+            res.send(503, new Error('Size can only be between 3 and 16.'));
+            return next();
+        }
+        if(tiles < 0 || tiles > (size*size/2)) {
+            res.send(503, new Error('Number of intial tiles can only be between 1 to size^2/2'));
+            return next();
+        }
+        if(victory <= rand || victory > 32) {
+            res.send(503, new Error('Victory power cannot be smaller than or equal to rand or be greater than 32'));
+            return next();
+        }
+        if(move < 0 || move > victory) {
+            res.send(503, new Error('Rand tiles has to be greater than 1 or less than victory'));
+            return next();
+        }
+    }
+
+    gameStates[session_id] = new Game(size,tiles,victory,rand);
+
+    var json = '';
+    if((req.url).match(/\/json\//)) {
+        json = '/json';
+    }
+
+    res.header('Location', '/state/' + session_id + json + '/thanks');
+    res.send(302);
     return next();
+}
 
-});
 
-server.get('/state/:session_id', function (req, res, next) {
-
-    //Retrieve session_id
+function gameMove(req,res,next) {
     var session_id = req.params.session_id;
 
-    //Get game state from local cache
-    var gameState = gameStates[session_id].getState();
-    //Success send over
-    gameState['session_id'] = session_id;
-    res.send(gameState);
-    return next();
-});
-
-server.get('/state/:session_id/move/:move', function (req, res, next) {
-
-    //Retrieve session_id
-    var session_id = req.params.session_id;
-    var move = parseInt(req.params.move);
-
-    if(move < 0 || move > 3) {
-        res.send(503, new Error('Invalid Move'));
+    //Check if session id exists
+    if(!(session_id in gameStates)) {
+        res.send(503, new Error('Invalid session id or session id has already expired.'));
         return next();
     }
 
     //Get game state from local cache
     var game = gameStates[session_id];
-    game.move(move);
+
+    //Check if move parameter was defined
+    if(!(typeof req.params.move === "undefined")) {
+        var move = parseInt(req.params.move);
+        if(move < 0 || move > 3) {
+            res.send(503, new Error('Invalid Move'));
+            return next();
+        }
+        game.move(move);
+    }
+
     var gameState = game.getState();
     gameState['session_id'] = session_id;
+    gameState['inspiration'] = starwars();
 
-    res.send(gameState);
+    var table = new Table({
+        chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+    });
+
+    for(var i in gameState['grid']) {
+        table.push(gameState['grid'][i]);
+    }
+
+    var str = 'Session ID: ' + session_id +  '\n';
+    str += 'Overall Score: ' + gameState['score'] +  '\n\n';
+    str += 'Grid:\n' + table.toString() + '\n\n';
+    str += 'Inspiration:\n' + gameState['inspiration'] + '\n';
+    console.log(str);
+
+    //JSON request, so send the gameState object
+    if((req.url).match(/\/json\//)) {
+        res.send(gameState);
+        return next();
+    }
+
+    res.contentType = 'text';
+    res.send(str);
+    return next();
+}
+
+//Routes
+server.get('/start/thanks', startGame);
+server.get('/start/json/thanks', startGame);
+server.get('/start/size/:size/tiles/:tiles/victory/:victory/rand/:rand/thanks', startGame);
+server.get('/start/size/:size/tiles/:tiles/victory/:victory/rand/:rand/json/thanks', startGame);
+
+server.get('/state/:session_id/thanks',gameMove);
+server.get('/state/:session_id/move/:move/thanks',gameMove);
+server.get('/state/:session_id/json/thanks',gameMove);
+server.get('/state/:session_id/move/:move/json/thanks',gameMove);
+
+server.get('/inspiration/thanks', function (req, res, next) {
+    var msg = {};
+    msg['inspiration'] = starwars();
+    res.send(obj);
     return next();
 });
 
@@ -66,18 +181,33 @@ server.listen(8080, function () {
 });
 
 function genSession() {
+    gameCount++;
     return crypto.createHash('sha1').update(uuid.v4()).digest('hex');
 }
+
+//Delete all game sessions that were inactive for 5 minutes
+function clearInactiveGames() {
+    var curTime = Math.round(+new Date()/1000);
+    for(var game_session in gameStates){
+        var game = gameStates[game_session];
+        if(game.getLastActive() < (curTime - TIMEOUT)) {
+            //Hackish way of keeping count
+           gameCount--;
+           delete gameStates[game_session];
+        }
+    }
+}
+
+setInterval (clearInactiveGames, TIMEOUT*100);
+
 //Catch SIGTERM and SIGINT and uncaughtException for graceful shutdown
 process.on( 'SIGTERM', function (err) {
     console.log('ERROR: Caught SIGTERM: ' + err);
-    client.quit();
     process.exit(1);
 });
 
 process.on( 'SIGINT', function (err) {
     console.log('ERROR: Caught SIGINT: ' + err);
-    client.quit();
     process.exit(1);
 });
 
